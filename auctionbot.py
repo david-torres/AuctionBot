a5c8bc50c77a5d2e4a57a11ddb42aaf06f31297a
David Torres <detour27@gmail.com>
2013-07-05 22:34:32 -0400

    stop flooding on restock

--- auctionbot.py
+++ auctionbot.py
@@ -800,6 +800,7 @@ def restock_items(message):
         if card_type['rarity'] > 0:
             text = 'Stocked: ' + card_type['name']
             scrolls.send({'msg': 'RoomChatMessage', 'roomName': room, 'text': text})
+            time.sleep(1)
         scrolls.send({'msg': 'LibraryView'})
         logging.info('Stocked: ' + card_type['name'] + ', card id: ' + str(card['id']))
 
